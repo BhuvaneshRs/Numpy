f7b91a072046a7897f29591b11ca000aeb63fdf5
Bhuvaneshwara Ragavendra Srinivasan <132887722+BhuvaneshRs@users.noreply.github.com>
2023-09-16 12:33:26 +0530

    Creating_Array.py

---
+++ Creating_Array.py
@@ -0,0 +1,27 @@
+x=n.array((10,20,30))
+
+x
+array([10, 20, 30])
+
+type(x)
+numpy.ndarray
+
+x.size
+3
+
+x.ndim
+1
+
+x.nbytes
+12
+
+
+y=n.array(((10,20,30),(40,50,60)))
+
+y
+array([[10, 20, 30],
+       [40, 50, 60]])
+
+
+y.ndim
+2
